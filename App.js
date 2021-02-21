3639f1636597856fd10ee050f4a8608da15e07cb
Phan Hieu Nguyen <fanhieu94@gmail.com>
2021-02-20 23:47:26 -0500

    added animated logo

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  Animated,
+  Easing,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
 import DealList from './components/DealList';
 import DealDetail from './components/DealDetail';
 import SearchBar from './components/SearchBar';
 import util from './util';
 
 export default class App extends React.Component {
+  titlePos = new Animated.Value(0);
   state = {
     deals: [],
     searchedDeals: [],
     currentDealId: null,
   };
-  async componentDidMount() {
-    const deals = await util.fetchInitial();
-    // console.log('data', deals);
-    this.setState((prevState) => {
-      return { deals };
+  animateLogo = (direction = 1) => {
+    let width = Dimensions.get('window').width;
+    Animated.timing(this.titlePos, {
+      toValue: (width / 2 - 100) * direction,
+      duration: 1000,
+      easing: Easing.ease
+    }).start(({ finished }) => {
+      if (finished) this.animateLogo(direction * -1);
     });
+  };
+  async componentDidMount() {
+    this.animateLogo();
+    const deals = await util.fetchInitial();
+    this.setState({ deals });
   }
   selectDeal = (dealId) => {
     this.setState({
@@ -47,8 +63,11 @@ export default class App extends React.Component {
         />
       );
     }
-    const dealsToDisplay = this.state.searchedDeals.length > 0 ? this.state.searchedDeals : this.state.deals;
-    if (dealsToDisplay)
+    const dealsToDisplay =
+      this.state.searchedDeals.length > 0
+        ? this.state.searchedDeals
+        : this.state.deals;
+    if (dealsToDisplay.length > 0)
       return (
         <View style={styles.container}>
           <SearchBar onSearch={this.searchDeals} />
@@ -56,9 +75,9 @@ export default class App extends React.Component {
         </View>
       );
     return (
-      <View style={styles.container}>
+      <Animated.View style={[{ left: this.titlePos }, styles.logo]}>
         <Text style={styles.header}>Sales</Text>
-      </View>
+      </Animated.View>
     );
   }
 }
@@ -75,82 +94,9 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 40,
   },
-});
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import DealList from './components/DealList';
-import DealDetail from './components/DealDetail';
-import SearchBar from './components/SearchBar';
-import util from './util';
-
-export default class App extends React.Component {
-  state = {
-    deals: [],
-    searchedDeals: [],
-    currentDealId: null,
-  };
-  async componentDidMount() {
-    const deals = await util.fetchInitial();
-    // console.log('data', deals);
-    this.setState((prevState) => {
-      return { deals };
-    });
-  }
-  selectDeal = (dealId) => {
-    this.setState({
-      currentDealId: dealId,
-    });
-  };
-  unSelectDeal = () => {
-    this.setState({
-      currentDealId: null,
-    });
-  };
-  currentDeal = () => {
-    return this.state.deals.find(
-      (deal) => deal.key === this.state.currentDealId
-    );
-  };
-  searchDeals = async (searchTerm) => {
-    let searchedDeals = [];
-    if (searchTerm) searchedDeals = await util.searchByTerm(searchTerm);
-    this.setState({ searchedDeals });
-  };
-  render() {
-    if (this.state.currentDealId) {
-      return (
-        <DealDetail
-          initialDeal={this.currentDeal()}
-          onBack={this.unSelectDeal}
-        />
-      );
-    }
-    const dealsToDisplay = this.state.searchedDeals.length > 0 ? this.state.searchedDeals : this.state.deals;
-    if (dealsToDisplay)
-      return (
-        <View style={styles.container}>
-          <SearchBar onSearch={this.searchDeals} />
-          <DealList data={dealsToDisplay} onPress={this.selectDeal} />
-        </View>
-      );
-    return (
-      <View style={styles.container}>
-        <Text style={styles.header}>Sales</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
+  logo: {
     flex: 1,
-    // justifyContent: 'center',
-    // backgroundColor: '#eee',
-    // alignItems: 'center',
-    paddingTop: 20,
-    // marginTop: 20,
-  },
-  header: {
-    fontSize: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
