ca923e6799806396d30e67bad945406703f8c40b
Phan Hieu Nguyen <fanhieu94@gmail.com>
2021-02-21 15:37:15 -0500

    added gesture handle for image slideshow

--- components/DealDetail.js
+++ components/DealDetail.js
@@ -1,10 +1,69 @@
 import * as React from 'react';
-import { Text, View, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+  Animated,
+  PanResponder,
+  Dimensions,
+  StyleSheet,
+  Linking,
+} from 'react-native';
 import util from '../util';
 
 export default class DealItem extends React.Component {
+  width = Dimensions.get('window').width;
+  imageXPos = new Animated.Value(0);
   state = {
     deal: this.props.initialDeal,
+    imageIndex: 0,
+  };
+  swipeResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (evt, gs) => {
+      this.imageXPos.setValue(gs.dx);
+    },
+    onPanResponderRelease: (evt, gs) => {
+      // 0.4 : if image swiped by 40% of screen dimension
+      // gs.dx : position of pan being dragged
+      if (Math.abs(gs.dx / this.width) > 0.4) {
+        // dx < 0 means swipe left, and reverse
+        const direction = Math.sign(gs.dx);
+        Animated.timing(this.imageXPos, {
+          toValue: direction * this.width,
+          duration: 250,
+        }).start(() => this.handleSwipe(direction * -1));
+      } else {
+        this.animationCancel();
+      }
+    },
+  });
+  handleSwipe = (indexDirection) => {
+    // swipe left means direction < 0, but image will be incremented, so index is direction reversed
+    if (this.state.deal.media[this.state.imageIndex + indexDirection]) {
+      this.setState(
+        (prevState) => {
+          return { imageIndex: prevState.imageIndex + indexDirection };
+        },
+        () => {
+          // next image animation - reset this position of new image
+          this.imageXPos.setValue(indexDirection * this.width);
+          Animated.spring(this.imageXPos, {
+            toValue: 0,
+          }).start();
+        }
+      );
+      console.log('relase');
+    } else {
+      this.animationCancel();
+    }
+  };
+  animationCancel = () => {
+    Animated.spring(this.imageXPos, {
+      toValue: 0,
+    }).start();
   };
   async componentDidMount() {
     const dealDetail = await util.fetchDealDetails(this.state.deal.key);
@@ -12,7 +71,9 @@ export default class DealItem extends React.Component {
       deal: dealDetail,
     });
   }
-
+  openExLink = () => {
+    Linking.openURL(this.state.deal.url);
+  };
   render() {
     const { deal } = this.state;
     return (
@@ -20,7 +81,11 @@ export default class DealItem extends React.Component {
         <TouchableOpacity onPress={this.props.onBack}>
           <Text style={styles.goBack}>Back</Text>
         </TouchableOpacity>
-        <Image style={styles.images} source={{ uri: deal.media[0] }} />
+        <Animated.Image
+          {...this.swipeResponder.panHandlers}
+          style={[{ left: this.imageXPos }, styles.images]}
+          source={{ uri: deal.media[this.state.imageIndex] }}
+        />
         <Text style={styles.title}>{deal.title}</Text>
         <View style={styles.info}>
           <View style={styles.subtitle}>
@@ -37,6 +102,7 @@ export default class DealItem extends React.Component {
         <View style={styles.description}>
           <Text>{deal.description}</Text>
         </View>
+        <Button title="Buy it from provider" onPress={this.openExLink} />
       </View>
     );
   }
